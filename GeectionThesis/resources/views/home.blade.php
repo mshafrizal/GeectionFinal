3653113daed0fc3f77f3598f35fd8cfdb70d6b7b
MUHAMMAD SHAFRIZAL <muhammad.shafrizal@binus.ac.id>
2018-05-22 13:05:35 +0700

    make auth

--- GeectionThesis/resources/views/home.blade.php
+++ GeectionThesis/resources/views/home.blade.php
@@ -38,34 +38,34 @@
             <hr>
             <div class="row">
                 <div class="col-sm-3">
-                    <div class="card" style="width:15rem; margin:8px;">
+                    <div class="card" style="max-width:15rem; margin:8px;">
                         <img src="{{ asset('img/kelli-tungay.png') }}" alt="Card image cap" class="card-img-top">
                         <div class="card-body text-center">
-                            <h6 class="card-title">Kategori</h6>
+                            <h6 class="card-title" style="font-size:1.5vw;">Kategori</h6>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <div class="card" style="width:15rem; margin:8px;">
+                    <div class="card" style="max-width:15rem; margin:8px;">
                         <img src="{{ asset('img/kelli-tungay.png') }}" alt="Card image cap" class="card-img-top">
                         <div class="card-body text-center">
-                            <h6 class="card-title">Kategori</h6>
+                            <h6 class="card-title" style="font-size:1.5vw;">Kategori</h6>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <div class="card" style="width:15rem; margin:8px;">
+                    <div class="card" style="max-width:15rem; margin:8px;">
                         <img src="{{ asset('img/kelli-tungay.png') }}" alt="Card image cap" class="card-img-top">
                         <div class="card-body text-center">
-                            <h6 class="card-title">Kategori</h6>
+                            <h6 class="card-title" style="font-size:1.5vw;">Kategori</h6>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <div class="card" style="width:15rem; margin:8px;">
+                    <div class="card" style="max-width:15rem; margin:8px;">
                         <img src="{{ asset('img/kelli-tungay.png') }}" alt="Card image cap" class="card-img-top">
                         <div class="card-body text-center">
-                            <h6 class="card-title">Kategori</h6>
+                            <h6 class="card-title" style="font-size:1.5vw;">Kategori</h6>
                         </div>
                     </div>
                 </div>
@@ -78,38 +78,38 @@
             <hr>
             <div class="row">
                 <div class="col-sm-3">
-                    <div class="card" style="width:15rem; margin:8px;">
+                    <div class="card" style="max-width:15rem; margin:8px;">
                         <img src="{{ asset('img/kelli-tungay.png') }}" alt="Card image cap" class="card-img-top">
                         <div class="card-body text-center">
-                            <h5 class="card-title">Ambil title dr DB</h5>
-                            <h6 class="card-title">Ambil harga dr DB</h6>
+                            <h5 class="card-title" style="font-size:2vw;">Ambil title dr DB</h5>
+                            <h6 class="card-title" style="font-size:1.5vw;">Ambil harga dr DB</h6>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <div class="card" style="width:15rem; margin:8px;">
+                    <div class="card" style="max-width:15rem; margin:8px;">
                         <img src="{{ asset('img/kelli-tungay.png') }}" alt="Card image cap" class="card-img-top">
                         <div class="card-body text-center">
-                            <h5 class="card-title">Ambil title dr DB</h5>
-                            <h6 class="card-title">Ambil harga dr DB</h6>
+                            <h5 class="card-title" style="font-size:2vw;">Ambil title dr DB</h5>
+                            <h6 class="card-title" style="font-size:1.5vw;">Ambil harga dr DB</h6>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <div class="card" style="width:15rem; margin:8px;">
+                    <div class="card" style="max-width:15rem; margin:8px;">
                         <img src="{{ asset('img/kelli-tungay.png') }}" alt="Card image cap" class="card-img-top">
                         <div class="card-body text-center">
-                            <h5 class="card-title">Ambil title dr DB</h5>
-                            <h6 class="card-title">Ambil harga dr DB</h6>
+                            <h5 class="card-title" style="font-size:2vw;">Ambil title dr DB</h5>
+                            <h6 class="card-title" style="font-size:1.5vw;">Ambil harga dr DB</h6>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <div class="card" style="width:15rem; margin:8px;">
+                    <div class="card" style="max-width:15rem; margin:8px;">
                         <img src="{{ asset('img/kelli-tungay.png') }}" alt="Card image cap" class="card-img-top">
                         <div class="card-body text-center">
-                            <h5 class="card-title">Ambil title dr DB</h5>
-                            <h6 class="card-title">Ambil harga dr DB</h6>
+                            <h5 class="card-title" style="font-size:2vw;">Ambil title dr DB</h5>
+                            <h6 class="card-title" style="font-size:1.5vw;">Ambil harga dr DB</h6>
                         </div>
                     </div>
                 </div>
--- GeectionThesis/routes/web.php
+++ GeectionThesis/routes/web.php
@@ -16,3 +16,7 @@ Route::get('/', function () {
 });
 
 // Route::get('/product', 'ProductController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
